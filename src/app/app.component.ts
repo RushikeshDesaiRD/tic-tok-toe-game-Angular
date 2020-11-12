daebb53da32b420b1d90956baec18f1c478ddad8
RushikeshDesaiRD <rushikeshdesaitwo@gmail.com>
2020-11-11 22:28:19 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'game';
+  winMessage:String=''
+  isCross:boolean=false
+  itemArray:string[]=new Array(9).fill('empty')
+ constructor(private tostr:ToastrService){
+
+ }
+ 
+
+ handleClick(itemNumber:number){
+   if(this.winMessage) {
+     return this.tostr.success("this.winMessage");
+   }
+   if(this.itemArray[itemNumber] === 'empty'){
+     this.itemArray[itemNumber] = this.isCross ? 'cross' : 'circle';
+     this.isCross=!this.isCross
+   }
+   else {
+     this.tostr.info('allrady Filled')
+   }
+   this.checkIsWinner()
+ }
+
+
+ checkIsWinner = () => {
+  //  checking  winner of the game
+  if (
+    this.itemArray[0] === this.itemArray[1] &&
+    this.itemArray[0] === this.itemArray[2] &&
+    this.itemArray[0] !== 'empty'
+  ) {
+    this.winMessage = `${this.itemArray[0]} won`;
+  } else if (
+    this.itemArray[3] !== 'empty' &&
+    this.itemArray[3] === this.itemArray[4] &&
+    this.itemArray[4] === this.itemArray[5]
+  ) {
+    this.winMessage = `${this.itemArray[3]} won`;
+  } else if (
+    this.itemArray[6] !== 'empty' &&
+    this.itemArray[6] === this.itemArray[7] &&
+    this.itemArray[7] === this.itemArray[8]
+  ) {
+    this.winMessage = `${this.itemArray[6]} won`;
+  } else if (
+    this.itemArray[0] !== 'empty' &&
+    this.itemArray[0] === this.itemArray[3] &&
+    this.itemArray[3] === this.itemArray[6]
+  ) {
+    this.winMessage = `${this.itemArray[0]} won`;
+  } else if (
+    this.itemArray[1] !== 'empty' &&
+    this.itemArray[1] === this.itemArray[4] &&
+    this.itemArray[4] === this.itemArray[7]
+  ) {
+    this.winMessage = `${this.itemArray[1]} won`;
+  } else if (
+    this.itemArray[2] !== 'empty' &&
+    this.itemArray[2] === this.itemArray[5] &&
+    this.itemArray[5] === this.itemArray[8]
+  ) {
+    this.winMessage = `${this.itemArray[2]} won`;
+  } else if (
+    this.itemArray[0] !== 'empty' &&
+    this.itemArray[0] === this.itemArray[4] &&
+    this.itemArray[4] === this.itemArray[8]
+  ) {
+    this.winMessage = `${this.itemArray[0]} won`;
+  } else if (
+    this.itemArray[2] !== 'empty' &&
+    this.itemArray[2] === this.itemArray[4] &&
+    this.itemArray[4] === this.itemArray[6]
+  ) {
+    this.winMessage = `${this.itemArray[2]} won`;
+  }
+};
+ reloadGame(){
+   this.winMessage='',
+   this.isCross=false,
+   this.itemArray=new Array[9].fill('empty')
+
+
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './componens/icon/icon.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
+   
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
